4e5302a448e3d3e1b6e81acfb76143be10c01c11
aki-0517 <akki.pp.777@gmail.com>
2024-12-05 11:56:28 +0900

    feat: ExpirableERC721

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,44 +1,35 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { Contract } from "ethers";
 
 /**
- * Deploys a contract named "YourContract" using the deployer account and
+ * Deploys a contract named "ExpirableERC721" using the deployer account and
  * constructor arguments set to the deployer address
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  /*
-    On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
-
-    When deploying to live networks (e.g `yarn deploy --network sepolia`), the deployer account
-    should have sufficient balance to pay for the gas fees for contract creation.
-
-    You can generate a random account with `yarn generate` which will fill DEPLOYER_PRIVATE_KEY
-    with a random private key in the .env file (then used on hardhat.config.ts)
-    You can run the `yarn account` command to check your balance in every network.
-  */
+const deployExpirableERC721: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment
+) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  // コントラクトのコンストラクタ引数を設定
+  const name = "YourTokenName";
+  const symbol = "YTN";
+  const initialOwner = deployer;
+
+  await deploy("ExpirableERC721", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: [name, symbol, initialOwner],
     log: true,
-    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-    // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
   });
 
-  // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  // デプロイされたコントラクトを取得して、必要に応じて操作を行う
+  const ExpirableERC721 = await hre.ethers.getContract("ExpirableERC721", deployer);
+  console.log("ExpirableERC721 deployed at:", ExpirableERC721.getAddress());
 };
 
-export default deployYourContract;
-
-// Tags are useful if you have multiple deploy files and only want to run one of them.
-// e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+export default deployExpirableERC721;
+deployExpirableERC721.tags = ["ExpirableERC721"];
