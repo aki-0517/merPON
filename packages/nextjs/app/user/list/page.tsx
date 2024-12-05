c479ef3f845b3d16ec0b97c9ab9973a63163c567
aki-0517 <akki.pp.777@gmail.com>
2024-12-05 15:22:41 +0900

    add CardList

--- packages/nextjs/app/user/list/page.tsx
+++ packages/nextjs/app/user/list/page.tsx
@@ -1,25 +1,39 @@
 import type { NextPage } from "next";
-import { Card } from "~~/components/Card";
+import { CardList } from "~~/components/CardList";
 
 const UserListPage: NextPage = () => {
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
-        gap: "16px",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "70%",
-        margin: "100px auto",
-      }}
-    >
-      <Card title="引換券" issueDate="2024.12.05" amount={7000} expiryDate="2025.06.30" usageScope="店舗全体" />
-      <Card title="引換券" issueDate="2024.12.05" amount={7000} expiryDate="2025.06.30" usageScope="店舗全体" />
-      <Card title="引換券" issueDate="2024.12.05" amount={7000} expiryDate="2025.06.30" usageScope="店舗全体" />
-      <Card title="引換券" issueDate="2024.12.05" amount={7000} expiryDate="2025.06.30" usageScope="店舗全体" />
-    </div>
-  );
+  const cards = [
+    {
+      title: "引換券",
+      issueDate: "2024.12.05",
+      amount: 7000,
+      expiryDate: "2025.06.30",
+      usageScope: "店舗全体",
+    },
+    {
+      title: "引換券",
+      issueDate: "2024.12.05",
+      amount: 7000,
+      expiryDate: "2025.06.30",
+      usageScope: "店舗全体",
+    },
+    {
+      title: "引換券",
+      issueDate: "2024.12.05",
+      amount: 7000,
+      expiryDate: "2025.06.30",
+      usageScope: "店舗全体",
+    },
+    {
+      title: "引換券",
+      issueDate: "2024.12.05",
+      amount: 7000,
+      expiryDate: "2025.06.30",
+      usageScope: "店舗全体",
+    },
+  ];
+
+  return <CardList cards={cards} />;
 };
 
 export default UserListPage;
